0f473849bb1cf8adb0a4d4a1f954d5ec2d559db2
Antony Sanders <antony.devon92@gmail.com>
2020-01-10 15:14:39 +0000

    first commit

---
+++ lib/top50singers/cli.rb
@@ -0,0 +1,8 @@
+require "top50singers/cli/version"
+
+module Top50singers
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/top50singers/cli/version.rb
@@ -0,0 +1,5 @@
+module Top50singers
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
